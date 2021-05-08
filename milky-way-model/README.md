96da2434daaebe05ecb972a29a15ce0ff5ecf40c
Pedro Vicente <pe.vicentee@gmail.com>
2021-05-07 23:40:23 -0400

    Starter file for volcano sim.

--- milky-way-model/README.md
+++ milky-way-model/README.md
@@ -1,2 +1,3 @@
 # milky-way-model
 2D graphical representation of the Milky Way projecting the probability of any civilization detecting the radio transmissions of any other civilization by using the Drake equation.
+
---
+++ volcano-simulator/tvashtar.py
@@ -0,0 +1,13 @@
+import sys
+import math
+import random
+import pygame as pg
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+LT_GREY = (180, 180, 180)
+GREY = (120, 120, 120)
+DK_GREY = (80, 80, 80)
+
+if __name__ == "__main__":
+    pg.init()
